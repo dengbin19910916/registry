2082c3b912e275c1c0634b955783473a1d794efc
dengbin <dengbin@meicloud.com>
2019-01-10 16:22:28 +0800

    Initial commit

---
+++ src/main/java/com/willowleaf/registry/RegistryApplication.java
@@ -0,0 +1,18 @@
+package com.willowleaf.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableEurekaServer
+@EnableHystrixDashboard
+@SpringBootApplication
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+
+}
+
